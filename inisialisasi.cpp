d58b425f9100833cff4e78cab5f0feb7c2e8352a
PannnTastic <pannntastic@gmail.com>
2024-05-21 17:38:33 +0700

    destructor

--- inisialisasi.cpp
+++ inisialisasi.cpp
@@ -1,14 +1,24 @@
 #include <iostream>
 #include <string>
+using namespace std;
 
 class mahasiswa {
     private : 
-    const int id;
-    string nama;
-    float nilai;
+        const int id;
+        string nama;
+        float nilai;
 
     public :
         mahasiswa(int pid, string pnama, float pnilai) : id(pid), nama(pnama), nilai(pnilai){
-            
+
         }
+        ~mahasiswa(){
+            cout << "id    = " << id << endl;
+            cout << "Nama  = " << nama << endl;
+            cout << "Nilai = " << nilai << endl;
+        }
+};
+
+int main(){
+    
 }
