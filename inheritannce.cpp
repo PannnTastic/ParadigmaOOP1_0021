f98bf0a8b513b781daa5e0e28fabe13438c37285
PannnTastic <pannntastic@gmail.com>
2024-05-21 19:44:58 +0700

    program utama

--- inheritannce.cpp
+++ inheritannce.cpp
@@ -42,3 +42,10 @@ public:
     }
 };
 
+int main()
+{
+    peLajar siswa1("andi laksono", "SMAN 1 Bantul");
+    cout << siswa1.perkenalan();
+    cout << "Hasil = " << siswa1.jumlah(10, 90) << endl;
+    return 0;
+}
