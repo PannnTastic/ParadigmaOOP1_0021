7254313fcf9b750d9a6238af4333a0d649562bbe
PannnTastic <pannntastic@gmail.com>
2024-05-21 19:28:01 +0700

    class pasien yang menggunakan vector

--- asosiasi.cpp
+++ asosiasi.cpp
@@ -3,3 +3,16 @@
 using namespace std;
 
 class dokter;
+class Pasien { 
+public:
+string nama;
+vector<dokter*> daftar_dokter;
+Pasien(string pNama)  : nama(pNama) {
+cout << "Pasien \"" << nama << "\" ada\n";
+}
+~Pasien() {
+cout << "Pasien \"" << nama << "\" tidak ada\n";
+}
+void tambahDokter(dokter*);
+void cetakPasien();
+};
