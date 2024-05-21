c6f8d81922e1ea9d4b1f0d4043e24aee8afaa318
PannnTastic <pannntastic@gmail.com>
2024-05-21 17:35:45 +0700

    member initilisation list

--- inisialisasi.cpp
+++ inisialisasi.cpp
@@ -1,2 +1,14 @@
 #include <iostream>
 #include <string>
+
+class mahasiswa {
+    private : 
+    const int id;
+    string nama;
+    float nilai;
+
+    public :
+        mahasiswa(int pid, string pnama, float pnilai) : id(pid), nama(pnama), nilai(pnilai){
+            
+        }
+}
