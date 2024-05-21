f51bb907eeac9437138ee8b49fddd9599a32abc9
PannnTastic <pannntastic@gmail.com>
2024-05-21 19:30:37 +0700

    class dokter

--- asosiasi.cpp
+++ asosiasi.cpp
@@ -3,16 +3,36 @@
 using namespace std;
 
 class dokter;
-class Pasien { 
+class Pasien
+{
 public:
-string nama;
-vector<dokter*> daftar_dokter;
-Pasien(string pNama)  : nama(pNama) {
-cout << "Pasien \"" << nama << "\" ada\n";
-}
-~Pasien() {
-cout << "Pasien \"" << nama << "\" tidak ada\n";
-}
-void tambahDokter(dokter*);
-void cetakPasien();
+    string nama;
+    vector<dokter *> daftar_dokter;
+    Pasien(string pNama) : nama(pNama)
+    {
+        cout << "Pasien \"" << nama << "\" ada\n";
+    }
+    ~Pasien()
+    {
+        cout << "Pasien \"" << nama << "\" tidak ada\n";
+    }
+    void tambahDokter(dokter *);
+    void cetakdokter();
+};
+
+class dokter
+{
+public:
+    string nama;
+    vector<Pasien *> daftar_pasien;
+    dokter(string pNama) : nama(pNama)
+    {
+        cout << "Dokter \"" << nama << "\" ada\n";
+    }
+    ~dokter()
+    {
+        cout << "Dokter \"" << nama << "\" tidak ada\n";
+    }
+    void tambahPasien(Pasien *);
+    void cetakPasien();
 };
