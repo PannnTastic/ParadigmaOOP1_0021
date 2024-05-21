3b582b76f8e9cba1b6bbf941a7c08ab719790947
PannnTastic <pannntastic@gmail.com>
2024-05-21 20:09:28 +0700

    merapikan kode dan menambahkan komen penting

--- ibu.h
+++ ibu.h
@@ -7,14 +7,16 @@ class ibu
 public:
     string nama;
     vector<anak *> daftar_anak;
+
     ibu(string pNama) : nama(pNama)
     {
-        cout << "Ibu " << nama << "\" ada\n ";
+        cout << "Ibu \"" << nama << "\" ada\n ";
     }
     ~ibu()
     {
-        cout << "Ibu " << nama << "\" tidak ada\n ";
+        cout << "Ibu \"" << nama << "\" tidak ada\n ";
     }
+
     void tambahAnak(anak *);
     void cetakAnak();
 };
@@ -26,14 +28,14 @@ void ibu::tambahAnak(anak *pAnak)
 
 void ibu::cetakAnak()
 {
-    cout << "Daftar Anak dari Ibu " << this->nama << "\" :\n ";
-        /
-        for (auto &a : daftar_anak)
-    {
-        cout << a->nama << "\n";
-    }
-    /
-        for (int i = 0; i < daftar_anak.size(); i++)
+    cout << "Daftar Anak dari Ibu \"" << this->nama << "\" :\n ";
+        
+    //     for (auto &a : daftar_anak)
+    // {
+    //     cout << a->nama << "\n";
+    // }
+    
+    for (vector<anak*> ::size_type i = 0; i < daftar_anak.size(); i++) //saya mengubah int menjadi seperti d samping d d karenakan ad pesan Warning dan saya mendapat solusi tersebut dri forum stackoverflow
     {
         cout << daftar_anak[i]->nama << endl;
     }
